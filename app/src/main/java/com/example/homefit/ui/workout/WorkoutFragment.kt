6513bc922472c940a6aa16e4511af2104e3043af
angie <angelica.ekman@hotmail.com>
2025-02-13 12:44:36 +0100

    UI for workout

---
+++ app/src/main/java/com/example/homefit/ui/workout/WorkoutFragment.kt
@@ -0,0 +1,31 @@
+package com.example.homefit.ui.workout
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.homefit.R
+
+class WorkoutFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = WorkoutFragment()
+    }
+
+    private val viewModel: WorkoutViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_workout, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/homefit/ui/workout/WorkoutViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.homefit.ui.workout
+
+import androidx.lifecycle.ViewModel
+
+class WorkoutViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

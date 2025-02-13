4ae947ea099c9610d0e9eb919a232b292678e337
Saeid-hub <iamsaeidahmadi@gmail.com>
2025-02-13 21:40:27 +0100

    Lagt till och fixat meny i appen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
 
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation("androidx.navigation:navigation-compose:2.5.3") // Navigering

b13288ccf146302306296f66e9990f28a6174451
BumBaZo96 <lina.bergsten86@live.se>
2025-02-13 13:49:58 +0100

    Profile1

--- app/src/main/java/com/example/homefit/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/example/homefit/ui/signin/SignInFragment.kt
@@ -12,6 +12,7 @@ import com.example.homefit.R
 import com.example.homefit.databinding.FragmentSignInBinding
 import com.example.homefit.ui.viewmodelauth.AuthViewModel
 
+
 class SignInFragment : Fragment() {
 
     // Binding för att hantera fragmentets layout från xml

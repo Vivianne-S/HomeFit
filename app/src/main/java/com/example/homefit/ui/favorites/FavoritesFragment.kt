6d0630280cb1b78c73b8c0abee4f56313d267567
Saeid-hub <iamsaeidahmadi@gmail.com>
2025-02-20 11:52:31 +0100

    Lagt till UI för favoritsidan

---
+++ app/src/main/java/com/example/homefit/ui/favorites/FavoritesFragment.kt
@@ -0,0 +1,35 @@
+package com.example.homefit.ui.favorites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.homefit.R
+import com.example.homefit.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Om det inte finns favoriter, visa meddelandet
+        binding.tvNoFavorites.visibility = View.VISIBLE
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
